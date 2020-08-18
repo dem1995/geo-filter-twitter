3d95bee0b8bd448efe9d328c0b66e7e1f8309cff
dem1995 <belmont95@live.com>
2020-08-18 13:30:29 -0500

    Making the read-in of the specifications file more robust; making things more clear inside of it through 'comments' in the file; replacing . with _ when location names are parsed to be used as file names

--- preparation-generation_script.sh
+++ preparation-generation_script.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 # Script to generate a list of tweet datasets centered around coordinates provided by a file.
 
-RADIUS=40
+tmpfile=$(mktemp /tmp/abc-script.XXXXXX)
 
-while IFS= read -r LINE || [ -n "$LINE" ];
+RADIUS=80
+cat /dev/null > generate_datasets.sh
+sed '/^#/ d' < preparation-input_cities.txt | while IFS= read -r LINE || [ -n "$LINE" ];
 do
-	COORDS="$(python city_to_latlong.py "$LINE")"
-	VALID_FILENAME=$(echo "$LINE" | sed -e 's/[^A-Za-z0-9._-]/_/g')
-	echo "python filter_latlong_radius.py tests/out_test.jsonl '"$COORDS"' "$RADIUS" > "$VALID_FILENAME"_"$RADIUS"" >> generate_datasets.sh
-done < preparation-input_cities.txt
+	if echo $LINE | egrep -q '^([0-9]+(\.[0-9][0-9]?)?)'; then
+		RADIUS=$(echo $LINE | sed -e 's/\r//g')
+	else
+		COORDS="$(python city_to_latlong.py "$LINE")"
+		VALID_FILENAME=$(echo "$LINE" | sed -e 's/[^A-Za-z0-9_-]/_/g')
+		echo "python filter_latlong_radius.py tests/out_test.jsonl '"$COORDS"' "$RADIUS" > "$VALID_FILENAME"_"$RADIUS"" >> generate_datasets.sh
+	fi
+done
