aaf8a1fbbfe035a455e25320409e35176711ea29
Dawn McKnight <dem@gra-login3.graham.sharcnet>
2020-09-22 08:26:43 -0400

    Removing the generate_datasets.sh script, which is created when preparation-generation_script.sh is run

--- generate_datasets.sh
+++
@@ -1,8 +0,0 @@
-python filter_latlong_radius.py tests/out_test.jsonl '(43.6534817, -79.3839347)' 40 > Toronto__ON__40
-python filter_latlong_radius.py tests/out_test.jsonl '(53.535411, -113.507996)' 40 > Edmonton__AB__40
-python filter_latlong_radius.py tests/out_test.jsonl '(51.0534234, -114.0625892)' 40 > Calgary__AB__40
-python filter_latlong_radius.py tests/out_test.jsonl '(49.2608724, -123.1139529)' 40 > Vancouver__BC__40
-python filter_latlong_radius.py tests/out_test.jsonl '(45.4972159, -73.6103642)' 40 > Montreal__QC__40
-python filter_latlong_radius.py tests/out_test.jsonl '(40.7127281, -74.0060152)' 40 > New_York_City__NY__40
-python filter_latlong_radius.py tests/out_test.jsonl '(30.2711286, -97.7436995)' 40 > Austin__TX__40
-python filter_latlong_radius.py tests/out_test.jsonl '(38.6268039, -90.1994097)' 40 > St__Louis__MO_40
