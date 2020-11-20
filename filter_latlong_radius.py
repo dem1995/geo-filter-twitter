ef583a6f888be1accae2cdc675b11ef66774c58d
Dawn McKnight <demcknig@ualberta.ca>
2020-11-20 04:28:26 -0600

    adding some additional checks - apparently Twitter can remove places after they've been created.

--- filter_latlong_radius.py
+++ filter_latlong_radius.py
@@ -13,16 +13,25 @@ from geopy.geocoders import Nominatim
 def is_tweet_in_latlong_radius(tweet_json, latlong, radius):
 	tweet_dict = json.loads(tweet_json)
 	tweet_coords = None
+	
+	#Check whether coordinates are available or whether a valid place is available to approximate coordinates from
 	coordinates_available = 'coordinates' in tweet_dict and tweet_dict['coordinates'] is not None
-	place_available = 'place' in tweet_dict and tweet_dict['place'] is not None
+	place_available = ('place' in tweet_dict and tweet_dict['place'] is not None
+			   and 'bounding_box' in tweet_dict['place']
+			   and tweet_dict['bounding_box']['place'] is not None
+			   and 'coordinates' in tweet_dict['bounding_box']['place']
+			   and tweet_dict['bounding_box']['place']['coordinates'] is not None)
+	
+	#If coordinates are available in some form, use them as the tweet coordinates
 	if coordinates_available:
 		tweet_coords = tweet_dict['coordinates']['coordinates']
 	elif place_available:
 		tweet_bounding_box = tweet_dict['place']['bounding_box']['coordinates']
 		tweet_bb_centroid = elmy.quotient(elmy.sum(tweet_bounding_box), [len(tweet_bounding_box)]*2)
 		tweet_coords = list(tweet_bb_centroid)
-	assert(tweet_coords is not None)
-	#longitude is currently first; this should be flipped
+	assert tweet_coords is not None, f"Tweet coords is none. Offending tweet: {tweet_json}"
+	
+	#longitude is currently first due to how Twitter orders coordinates; we flip this now
 	tweet_coords = list(reversed(tweet_coords))
 	tweet_distance = distance(tweet_coords, latlong)
 	return tweet_distance.miles <= radius
@@ -48,25 +57,3 @@ if __name__ == "__main__":
 		for tweet_json in infile:
 			if is_tweet_in_latlong_radius(tweet_json, coords, args.radius):
 				print(tweet_json[0:-1])
-
-
-# #def centroid(List[])
-
-# geolocator = Nominatim(user_agent="filter-latlong.py")
-# print(geolocator.geocode("Saint-Louis, Canada").point)
-# _, coord2 = geolocator.geocode("saint louis")
-# _, coord1 = geolocator.geocode("toronto")
-# print(coord2)
-# print(coord1)
-# print(distance(coord1, coord2))
-
-# if __name__ == "__main__":
-# 	with open("out_test.jsonl") as infile:
-# 		for line in infile:
-# 			_, chicago = geolocator.geocode("chicago")
-# 			print(is_tweet_in_latlong_radius(line, chicago, 10))
-
-# # 	parser = argparse.ArgumentParser(desc=__doc__)
-
-
-# # 	print("Enter the city that you want to ")
